dd44da0d4f8888463012f4fa8d31a6e0f91bc46f
Jeff Peiffer <jpeiffer@gmail.com>
2020-06-01 18:09:55 -0400

    Removed unused import.

--- lib/src/platform/browser/browser_svg.dart
+++ lib/src/platform/browser/browser_svg.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 
-import 'package:convert/convert.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
